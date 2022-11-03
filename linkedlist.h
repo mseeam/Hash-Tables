23f71b788436f5fcad585b0e5e16065281d23947
mseeam <mseeam99@gmail.com>
2022-11-03 14:19:08 -0500

    linkedlist.h

---
+++ linkedlist.h
@@ -0,0 +1,32 @@
+/********************************************************************************
+ * File  :   linkedlist.h                                                       *
+ * Author: Mohammad Seeam                                                       *
+ * Assignment 6: Linked List                                                    *
+ * Create a fully functional doubly linked list class                           *
+ * This header structure for all cpp and h files.                               *
+ * This file, linkedlist.h includes only data.h                                 *
+ *******************************************************************************/
+
+#include "data.h"
+
+class LinkedList {
+
+public:
+
+    LinkedList();    //constractor
+    ~LinkedList();   //destractor
+
+    //Declaring all the function
+    bool addNode(int, string*);
+    bool deleteNode(int); 
+    bool getNode(int, Data*);
+    bool clearList();
+    bool exists(int); 
+    int getCount();
+    void printList(bool=false);
+
+private:
+
+    Node* head;
+
+};
